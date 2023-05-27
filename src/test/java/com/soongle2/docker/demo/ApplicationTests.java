b4c267e6c15426882473385389dd989503371cd9
JIH0LEE <skql5810@gmail.com>
2023-05-27 21:00:47 +0900

    init

---
+++ src/test/java/com/soongle2/docker/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soongle2.docker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
